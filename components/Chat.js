d0c94d80212c140a492b3301b333d02d4252b5cd
AHenry95 <ahenry.dev@gmail.com>
2025-09-08 19:45:12 -0400

    Import and Implement Gifted Chat for chat feature

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,47 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation}) => {
+    const [messages, setMessages] = useState([]);
     const { name, backgroundColor } = route.params;
+
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages));
+    };
 
     useEffect(() => {
         navigation.setOptions({ title: name });
     }, []);
 
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any'
+                }
+            }
+        ]);
+    }, []);
+
     return (
         <View style={[styles.container, { backgroundColor: backgroundColor }]}>
-            <Text>Hello {name}!</Text>
+            <GiftedChat
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1
+                }}
+            />
+            { Platform.OS === 'android'
+                ? <KeyboardAvoidingView behavior="height" />
+                : null
+            }
         </View>
     );
 }
